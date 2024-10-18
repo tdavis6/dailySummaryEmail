8d3913469ce9d5c9273e6452866a270d5857fc19
Tyler Davis <coding@tydavis.dev>
2024-10-18 16:53:22 -0700

    fix #24

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from get_timezone import get_timezone
 from get_todo_tasks import get_todo_tasks
 from send_email import send_email
 
-VERSION = "0.1.0 (8)"
+VERSION = "0.1.0 (9)"
 
 # Load the environment variables from the .env file
 load_dotenv()
