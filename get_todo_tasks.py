db9f53d76093663b63aa9348b8f381cdd430ee47
Tyler Davis <coding@tydavis.dev>
2024-10-18 11:53:57 -0700

    Update a text add func

--- get_todo_tasks.py
+++ get_todo_tasks.py
@@ -3,6 +3,6 @@ from get_todoist_tasks import get_todoist_tasks
 def get_todo_tasks(TIMEZONE, TODOIST_API_KEY = None):
     text = """"""
     if TODOIST_API_KEY:
-        text = text + "\n\n# Tasks"
+        text += "\n\n# Tasks"
         text += get_todoist_tasks(TODOIST_API_KEY, TIMEZONE)
     return text
