3989ca5d0895d9ccf82e4eda99662c339d59ea8f
Tyler Davis <coding@tydavis.dev>
2024-10-27 15:02:04 -0500

    fix #33
    Signed-off-by: Tyler Davis <coding@tydavis.dev>

--- get_coordinates.py
+++ get_coordinates.py
@@ -5,10 +5,10 @@ from geopy.geocoders import Nominatim
 
 def get_coordinates(address):
     try:
-        geolocator = Photon()
+        geolocator = Nominatim(user_agent="dailySummaryEmail")
         location = geolocator.geocode(address)
         return location.latitude, location.longitude
     except GeocoderUnavailable:
-        geolocator = Nominatim(user_agent="dailySummaryEmail")
+        geolocator = Photon()
         location = geolocator.geocode(address)
         return location.latitude, location.longitude
