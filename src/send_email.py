91ecbdf76c4d83a66262d9ca3da3ac00e370d269
Tyler Davis <coding@tydavis.dev>
2024-11-12 10:10:14 -0600

    update dark mode header color
    Signed-off-by: Tyler Davis <coding@tydavis.dev>

--- src/send_email.py
+++ src/send_email.py
@@ -180,7 +180,7 @@ def send_email(
                         box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
                     }}
 
-                    .section h2 {{
+                    .section h1 {{
                         color: #bbdefb;
                     }}
 
