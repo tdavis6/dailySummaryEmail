eef1061998fd79f1c2d71edf465daa78f2339fb9
Tyler Davis <coding@tydavis.dev>
2025-01-06 09:52:40 -0800

    fix word search formatting, looks better now

--- src/get_puzzles.py
+++ src/get_puzzles.py
@@ -14,8 +14,8 @@ def get_puzzles():
 
     sudoku_string, sudoku_ans_string = gen_sudoku(3,(random.randint(40, 61)/100))
 
-    puzzles_string += "\n\n## Sudoku" + "\n```" + sudoku_string + "\n" + "```"
-    puzzles_ans_string += "\n\n## Sudoku" + "\n```" + sudoku_ans_string + "\n" + "```"
+    puzzles_string += "\n\n## Sudoku" + "\n```\n" + sudoku_string + "\n" + "```"
+    puzzles_ans_string += "\n\n## Sudoku" + "\n```\n" + sudoku_ans_string + "\n" + "```"
 
     word_search = WordSearch()
     word_search.random_words(10, secret=True, reset_size=True)
@@ -24,12 +24,12 @@ def get_puzzles():
 
     for line in word_search.puzzle:
         for word in line:
-            word_search_string += word + ""
+            word_search_string += word + " "
         word_search_string += "\n"
     for word in word_search.words:
         word_search_ans_string += f"{word.text.capitalize()} ({word.coordinates[0]}, {word.direction.name}) "
 
-    puzzles_string += "\n\n## Word Search" + "\n```" + word_search_string + "\n" + "```"
-    puzzles_ans_string += "\n\n## Word Search" + "\n```" + word_search_ans_string + "\n" + "```"
+    puzzles_string += "\n\n## Word Search" + "\n```\n" + word_search_string + "\n" + "```"
+    puzzles_ans_string += "\n\n## Word Search" + "\n```\n" + word_search_ans_string + "\n" + "```"
 
     return puzzles_string, puzzles_ans_string
