9446a650afb0c03948778a45f563215c61eac5a0
Tyler Davis <coding@tydavis.dev>
2025-05-30 22:43:30 -0700

    edit the todoist task filter to only include tasks that are assigned to the user or not assigned at all

--- src/get_todoist_tasks.py
+++ src/get_todoist_tasks.py
@@ -5,7 +5,7 @@ def get_todoist_tasks(TODOIST_API_KEY):
     api = TodoistAPI(TODOIST_API_KEY)
     try:
         # Fetch tasks that are due before tomorrow
-        todoist_data = api.get_tasks(filter="due before: tomorrow")
+        todoist_data = api.get_tasks(filter="due before: tomorrow & (assigned to: me | !assigned)")
         return todoist_data
     except Exception as e:
         logging.critical(f"Error occurred in get_todoist_tasks: {e}")
